2696e8099f418947091ce24bd2907d90e198a6d3
PhucTran <tranbaophuc2703@gmail.com>
2023-12-02 09:53:15 +0700

    update print

--- server/models/PrintStatus.js
+++ server/models/PrintStatus.js
@@ -6,9 +6,7 @@ async function showInfoPrintOder(user_id) {
       "SELECT name, status FROM print_order PO INNER JOIN customer C ON PO.document_id = C.document_id WHERE user_id = ? LIMIT 5;",
       [user_id]
     );
-    return {
-      status: "Success",
-    };
+    return result;
   } catch (err) {
     console.error(err);
     throw err;
