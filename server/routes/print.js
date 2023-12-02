8b01e5849ab7c1545dbf172bd28d9d834063ca73
PhucTran <tranbaophuc2703@gmail.com>
2023-12-02 22:29:23 +0700

    upload file

--- server/routes/print.js
+++ server/routes/print.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const printController = require('../controllers/PrintController');
-const authenticate = require('../middlewares/authenticate');
+const SaveFileUpload = require('../controllers/UploadFile');
+let path = require("path");
+
+// const authenticate = require('../middlewares/authenticate');
+
 
 // router.post('/confirm',authenticate, printController.addFileOrder);
 // router.get('/status',authenticate, printController.showStatus);
@@ -12,5 +16,7 @@ router.post('/confirm', printController.addFileOrder);
 router.get('/status', printController.showStatus);
 router.put('/minusPages',printController.MinusPages);
 router.get('/getInfoPrinter',printController.showInfoPrinter);
+router.post('/uploadTemporaryFile',SaveFileUpload.SaveTemporaryFile);
+router.post('/uploadPrintFile',SaveFileUpload.SavePrintFile);
 
 module.exports = router;
