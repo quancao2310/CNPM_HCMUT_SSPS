a54f6008a8665cb153d42a8127c5cd9abfad09fe
Thanggggg <nguyenquocthang0212@gmail.com>
2023-12-05 03:21:33 +0700

    Add Report

---
+++ server/models/GetReportYearMonth.js
@@ -0,0 +1,89 @@
+const db = require("../config/db");
+
+async function GetReportYearMonth(month=null, year){
+    if (month == null)
+    {
+        return GetReportYear(year);
+    }
+    return GetReportMonth(month,year);
+}
+
+async function GetReportYear(year)
+{
+    try{        
+        const [result, _] = await db.execute(
+            `SELECT
+                p.printer_id,
+                COUNT(DISTINCT po.print_id) AS total_orders,
+                SUM(CASE WHEN po.page_size = 'A3' THEN 
+                        CASE WHEN po.pages_to_be_printed = 'ALL' THEN CEIL (d.no_of_pages / po.pages_per_sheet / po.side)
+                             ELSE CEIL((SUBSTRING_INDEX(po.pages_to_be_printed, '-', -1) - SUBSTRING_INDEX(po.pages_to_be_printed, '-', 1) + 1) / po.pages_per_sheet / po.side)
+                        END
+                        ELSE 0 
+                    END) AS total_A3_pages,
+                SUM(CASE WHEN po.page_size = 'A4' THEN 
+                        CASE WHEN po.pages_to_be_printed = 'ALL' THEN CEIL (d.no_of_pages / po.pages_per_sheet / po.side)
+                             ELSE CEIL((SUBSTRING_INDEX(po.pages_to_be_printed, '-', -1) - SUBSTRING_INDEX(po.pages_to_be_printed, '-', 1) + 1) / po.pages_per_sheet / po.side)
+                        END
+                        ELSE 0 
+                    END) AS total_A4_pages
+            FROM
+                print_order po
+            INNER JOIN document d ON po.document_id = d.document_id
+            INNER JOIN printer p ON d.printer_id = p.printer_id
+            WHERE
+                YEAR(po.time_end) = ? AND po.status = 'success'
+            GROUP BY
+                p.printer_id;`,
+            [year]
+        );
+        
+        return result;
+        
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+async function GetReportMonth(month, year)
+{
+    try{
+        const [result, _] = await db.execute(
+            `SELECT
+                p.printer_id,
+                COUNT(DISTINCT po.print_id) AS total_orders,
+                SUM(CASE WHEN po.page_size = 'A3' THEN 
+                        CASE WHEN po.pages_to_be_printed = 'ALL' THEN CEIL (d.no_of_pages / po.pages_per_sheet / po.side)
+                             ELSE CEIL((SUBSTRING_INDEX(po.pages_to_be_printed, '-', -1) - SUBSTRING_INDEX(po.pages_to_be_printed, '-', 1) + 1) / po.pages_per_sheet / po.side)
+                        END
+                        ELSE 0 
+                    END) AS total_A3_pages,
+                SUM(CASE WHEN po.page_size = 'A4' THEN 
+                        CASE WHEN po.pages_to_be_printed = 'ALL' THEN CEIL (d.no_of_pages / po.pages_per_sheet / po.side)
+                             ELSE CEIL((SUBSTRING_INDEX(po.pages_to_be_printed, '-', -1) - SUBSTRING_INDEX(po.pages_to_be_printed, '-', 1) + 1) / po.pages_per_sheet / po.side)
+                        END
+                        ELSE 0 
+                    END) AS total_A4_pages
+            FROM
+                print_order po
+            INNER JOIN document d ON po.document_id = d.document_id
+            INNER JOIN printer p ON d.printer_id = p.printer_id
+            WHERE
+                YEAR(po.time_end) = ? AND MONTH(po.time_end) = ? AND po.status = 'success'
+            GROUP BY
+                p.printer_id;`,
+            [year,month]
+        );
+        
+        return result;
+    } catch (err) {
+        console.error(err);
+        throw err;
+    }
+}
+
+
+module.exports = {
+    GetReportYearMonth
+};
